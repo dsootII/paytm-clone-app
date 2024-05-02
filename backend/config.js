02b85bfdf8ea339c12cbf51f8d264123bd5b1a54
dsootII <dsootsecond@gmail.com>
2024-05-02 12:45:34 +0530

    fixed 'security' threat 1

--- backend/config.js
+++
@@ -1,4 +0,0 @@
-const JWT_SECRET = 'secret123';
-const MONGO_CONNECT_URL = 'mongodb+srv://dsoot53:OZabuxPLSr7YsNGQ@cluster0.nu5mcja.mongodb.net/paytm-users';
-
-module.exports = {JWT_SECRET, MONGO_CONNECT_URL};
