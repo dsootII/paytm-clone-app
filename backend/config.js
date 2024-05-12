aa4ed9988d7324605c8aa2eb2d265da6f7735b23
dsootII <dsootsecond@gmail.com>
2024-05-12 11:34:00 +0530

    de1

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+const JWT_SECRET = 'secret123';
+const MONGO_CONNECT_URL = 'mongodb+srv://dsoot53:OZabuxPLSr7YsNGQ@cluster0.nu5mcja.mongodb.net/paytm-users';
+
+module.exports = {JWT_SECRET, MONGO_CONNECT_URL};
